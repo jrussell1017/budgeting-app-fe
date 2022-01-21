3b1b988b2d25909958997ae316dc9f278245b71f
james russell <jamesrussell@pursuit.org>
2022-01-21 14:34:40 -0500

    set up front end, installed dependencies, deleted clutter from react-app being created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+// DEPENDENCIES
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+// PAGES
 import './App.css';
+
+// COMPONENTS
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+
+      </Router>
+      <main>
+        <Routes>
+          <Route path="/" element={} />
+        </Routes>
+      </main>
     </div>
   );
 }
